1a0b74a922bd4650fd691ce4516127e55a7e781e
Swarnabha-21 <swarnabha.pradhan@gmail.com>
2024-06-29 23:05:01 +0530

    Create home.js

---
+++ home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to InstiOlx</h1>
+      <p>Your one-stop solution for buying and selling products.</p>
+    </div>
+  );
+}
+
+export default Home;
